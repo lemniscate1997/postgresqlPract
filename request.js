95580a30349d826b5920852fbcc53fdfcaeee5b4
Lemniscate <46708976+lemniscate1997@users.noreply.github.com>
2019-02-12 16:59:54 +0530

    Update request.js

--- request.js
+++ request.js
@@ -4,10 +4,6 @@ const router = require('./service')
 const cors = require('cors')
 
 var app = express()
-var corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
@@ -19,6 +15,17 @@ app.get('/', function(req, res){
     res.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 app.use('/Users', cors(corsOptions), router)
+
+/*
+app.options('*', cors())
+app.use('/Users', router)
+
+*/
 
 app.listen(3000)
